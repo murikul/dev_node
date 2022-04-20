784b9a8296bb2e017842eebc25450e23fb4efd0e
murikul <murikul@naver.com>
2022-04-20 10:21:06 +0900

    js dir add

--- app/app.js
+++ app/app.js
@@ -10,7 +10,7 @@ const home = require("./src/routes/home");
 //앱 셋팅
 app.set("views","./src/views");
 app.set("view engine","ejs");
-
+app.use(express.static(`${__dirname}/src/public`));
 // use : 미들웨어를 등록해주는 메서드 
 app.use("/", home);
 
